d37e063704e9bb018526c73b39b376c803e03e97
Andres <andres.castaneda280040@gmail.com>
2022-05-27 08:08:48 -0500

    Part05-07

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -21,6 +21,8 @@ import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 /**
  * Learn how to deal with errors.
  *
@@ -33,14 +35,14 @@ public class Part07Errors {
 
 	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorResume((Function<? super Throwable, ? extends Mono<? extends User>>) User.SAUL);
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
 	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
+		return flux.onErrorResume( error -> Flux.just(User.SAUL,User.JESSE));
 	}
 
 //========================================================================================
@@ -48,7 +50,7 @@ public class Part07Errors {
 	// TODO Implement a method that capitalizes each user of the incoming flux using the
 	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> capitalizeUser(user)).map(GetOutOfHereException -> Exceptions.propagate(flux));
 	}
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
